677c8fdb345d6183148826ac4577aa82bc6fa9d3
brunocesarangst <bruno.angst@rede.ulbra.br>
2020-09-28 01:40:05 -0300

    iniciando api com overnightjs e express

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import './util/module-alias';
+import { Server } from '@overnightjs/core'
+import bodyParser from 'body-parser';
+import { ForcastController } from './controllers/forcast';
+import { Application } from 'express';
+
+export class SetupServer extends Server {
+
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public init(): void {
+    this.setupExpress();
+    this.SetupControllers();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json())
+  }
+
+  private SetupControllers(): void {
+    const forcastController = new ForcastController();
+
+    this.addControllers([forcastController]);
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+}
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,11 +1,11 @@
-// import { SetupServer } from '@src/server';
-// import supertest from 'supertest';
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
 
 // let server: SetupServer;
-// beforeAll(async () => {
-//   server = new SetupServer();
-//   await server.init();
-//   global.testRequest = supertest(server.getApp());
-// });
+beforeAll(async () => {
+  const server = new SetupServer();
+  await server.init();
+  global.testRequest = supertest(server.getApp());
+});
 
 // afterAll(async () => await server.close());
