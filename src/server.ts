e8b194dbb6b9d3bfb8929f86b2347384c7ad2be8
brunocesarangst <bruno.angst@rede.ulbra.br>
2020-09-30 18:12:40 -0300

    módulo de users - criando controller e model

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForcastController } from './controllers/forcast';
 import { Application } from 'express';
-import * as database from '@src/database'
+import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -14,7 +15,7 @@ export class SetupServer extends Server {
   public async init(): Promise<void> {
     this.setupExpress();
     this.SetupControllers();
-    await this.databaseSetup()
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -24,22 +25,27 @@ export class SetupServer extends Server {
   private SetupControllers(): void {
     const forcastController = new ForcastController();
     const beachesController = new BeachesController();
+    const usersController = new UsersController();
 
-    this.addControllers([forcastController, beachesController]);
+    this.addControllers([
+      forcastController,
+      beachesController,
+      usersController,
+    ]);
   }
 
   private async databaseSetup(): Promise<void> {
-    await database.connect()
+    await database.connect();
   }
 
   public async close(): Promise<void> {
-    await database.close()
+    await database.close();
   }
 
   public start(): void {
     this.app.listen(this.port, () => {
-      console.info('Server listening of port ' + this.port)
-    })
+      console.info('Server listening of port ' + this.port);
+    });
   }
 
   public getApp(): Application {
